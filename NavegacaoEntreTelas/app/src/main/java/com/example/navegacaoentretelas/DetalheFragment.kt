89306aac0af8660d479cb93fdc0606a932507529
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2022-03-11 20:21:19 -0300

    Code refactor e passar parâmetros para a tela detalhe

--- NavegacaoEntreTelas/app/src/main/java/com/example/navegacaoentretelas/DetalheFragment.kt
+++ NavegacaoEntreTelas/app/src/main/java/com/example/navegacaoentretelas/DetalheFragment.kt
@@ -7,13 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import com.example.navegacaoentretelas.R
-import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM3 = "param3"
 
 /**
  * A simple [Fragment] subclass.
@@ -23,11 +22,15 @@ private const val ARG_PARAM2 = "param2"
 class DetalheFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
+    private var param2: String? = null
+    private var param3: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+            param3 = it.getString(ARG_PARAM3)
         }
     }
 
@@ -38,16 +41,24 @@ class DetalheFragment : Fragment() {
         // Inflate the layout for this fragment
         val inflate = inflater.inflate(R.layout.fragment_detalhe, container, false)
 
-        val button = inflate.findViewById(R.id.button2) as Button
+        val button = inflate.findViewById<Button>(R.id.button2)
 
-        val textView = inflate.findViewById(R.id.textView) as TextView
+        val textView1 = inflate.findViewById<TextView>(R.id.txt1)
 
-        textView.text = param1
+        textView1.text = param1
+
+        val textView2 = inflate.findViewById<TextView>(R.id.txt2)
+
+        textView2.text = param2
+
+        val textView3 = inflate.findViewById<TextView>(R.id.txt3)
+
+        textView3.text = param3
 
         button.setOnClickListener{
             val fm = fragmentManager
             fm!!.beginTransaction()
-                .replace(R.id.fragmentContainer,PrincipalFragment(),"PrincipalFragment")
+                .replace(R.id.fragmentContainer, PrincipalFragment(),"PrincipalFragment")
                 .addToBackStack(null).commit()
         }
 
@@ -60,14 +71,18 @@ class DetalheFragment : Fragment() {
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @param param3 Parameter 3.
          * @return A new instance of fragment DetalheFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String) =
+        fun newInstance(param1: String, param2: String, param3: String) =
             DetalheFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                    putString(ARG_PARAM3, param3)
                 }
             }
     }
--- NavegacaoEntreTelas/app/src/main/java/com/example/navegacaoentretelas/PrincipalFragment.kt
+++ NavegacaoEntreTelas/app/src/main/java/com/example/navegacaoentretelas/PrincipalFragment.kt
@@ -7,12 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import com.example.navegacaoentretelas.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -21,15 +18,9 @@ private const val ARG_PARAM2 = "param2"
  */
 class PrincipalFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -40,14 +31,22 @@ class PrincipalFragment : Fragment() {
 
         val inflate = inflater.inflate(R.layout.fragment_principal, container, false)
 
-        val goToButton = inflate.findViewById(R.id.button) as Button
+        val goToButton = inflate.findViewById<Button>(R.id.button)
 
-        val texto = inflate.findViewById(R.id.editTextTextPersonName) as EditText
+        val texto1 = inflate.findViewById<EditText>(R.id.edt1)
+
+        val texto2 = inflate.findViewById<EditText>(R.id.edt2)
+
+        val texto3 = inflate.findViewById<EditText>(R.id.edt3)
 
         goToButton.setOnClickListener{
             val fm = fragmentManager
             fm!!.beginTransaction()
-                .replace(R.id.fragmentContainer,DetalheFragment.newInstance(texto.text.toString()),"DetalheFragment")
+                .replace(R.id.fragmentContainer, DetalheFragment.newInstance(
+                    texto1.text.toString(),
+                    texto2.text.toString(),
+                    texto3.text.toString(),
+                ),"DetalheFragment")
                 .addToBackStack(null).commit()
         }
 
@@ -59,18 +58,10 @@ class PrincipalFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment PrincipalFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PrincipalFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(param1: String) = PrincipalFragment().apply { }
     }
 }
